7e0da20b2f5b48e0f8130f16060f74bf4b0f8476
Jena <chiachun0511@gmail.com>
2019-06-11 10:26:14 +0800

    add html content

--- all.js
+++ all.js
@@ -1,19 +1,12 @@
 $.getJSON('https://data.kcg.gov.tw/dataset/a98754a3-3446-4c9a-abfc-58dc49f2158c/resource/48d4dfc4-a4b2-44a5-bdec-70f9558cd25d/download/yopendata1070622opendatajson-1070622.json', function(data) {
     //data is the JSON string
     
-data.forEach(function (x) {
-  document.getElementById('content').innerHTML +=
-    '<div class="content">' + x.Location + '</div>';
-})
-});
+// data.forEach(function (x) {
+//   document.getElementById('content').innerHTML +=
+//     '<div class="content">' + x.Location + '</div>';
+// })
+// });
 
-// $.ajax({
-//     url: 'https://data.kcg.gov.tw/dataset/a98754a3-3446-4c9a-abfc-58dc49f2158c/resource/48d4dfc4-a4b2-44a5-bdec-70f9558cd25d/download/yopendata1070622opendatajson-1070622.json',
-//     dataType: 'json',
-//     success: function(data) {
-//       console.log(data);
-//     }
-//   });
 
 
 
